feb03e56edd692730a3d40c9eb65e8f435bf1b63
Rahul Solanki <rahul.solanki.12032001@gmail.com>
2023-09-20 20:55:18 +0530

    fix: add revalidation

--- actions/(products)/get-product.tsx
+++ actions/(products)/get-product.tsx
@@ -4,8 +4,8 @@ const URL = `${process.env.NEXT_PUBLIC_API_URL}/products`
 
 const getProduct = async(productId:String):Promise<Product>=>{
      
-    const res = await fetch(`${URL}/${productId}`);
-
+    const res = await fetch(`${URL}/${productId}`,{ next: {revalidate:5000 }});
+    console.log(res)
     return res.json()
 }
 
