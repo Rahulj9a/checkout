48e96acb9ea43cdfa197c3a0f30e6a83d9939dbd
Rahul Solanki <rahul.solanki.12032001@gmail.com>
2023-09-20 20:54:32 +0530

    feat: build gallery to showcase product photo

--- app/(routes)/product/[productId]/page.tsx
+++ app/(routes)/product/[productId]/page.tsx
@@ -1,5 +1,6 @@
 import getProduct from "@/actions/(products)/get-product"
 import getProducts from "@/actions/(products)/getAllProducts"
+import Gallery from "@/components/gallery"
 import Container from "@/components/ui/container"
  
 import Info from "@/components/ui/info"
@@ -18,13 +19,14 @@ const ProductPage: React.FC<ProductPageProps> = async ({ params }) => {
     if (!product) {
         return null
     }
+    console.log(product)
     return (
-        <div>
+        <div className="bg-white">
             <Container>
-                <div className="border-2 px-4 py-10 sm:px-6 lg:px-8">
-                    <div className="lg:grid lg:grid-2 lg:items-start lg:gap-x-8">
-                         
-                        <div>
+                <div className=" px-4 py-10 sm:px-6 lg:px-8">
+                    <div className="lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-8">
+                         <Gallery images={product.image}/>
+                        <div className="mt-10 sm:mt-16 px-4 sm:px-0 lg:mt-0">
                             <Info data={product}/>
                         </div>
                     </div>
--- components/gallery/gallery-tab.tsx
+++ components/gallery/gallery-tab.tsx
@@ -1,15 +1,29 @@
-import { Image } from "@/types"
+import { Image as ImageType } from "@/types";
+import { Tab } from "@headlessui/react";
+import Image from "next/image";
 
-interface galleryProps{
-    data:Image[]
+interface galleryProps {
+  image: ImageType;
 }
 
-const Gallery:React.FC<galleryProps> = ({data = []})=>{
- return(
-    <div>
+const GalleryTab: React.FC<galleryProps> = ({ image }) => {
+  return (
+    <Tab className="relative flex aspect-square cursor-pointer items-center justify-center rounded-md bg-white ">
+      {({ selected }) => (
+        <div>
+          <span className="absolute h-full w-full aspect-square inset-0 overflow-hidden rounded-md">
+            <Image
+              fill
+              src={image.url}
+              alt=""
+              className="object-cover object-center"
+            />
+          </span>
+          <span className={`absolute inset-0 rounded-md ring-2 ring-offset-2 ${selected?"ring-black": "ring-transparent"}`} />
+        </div>
+      )}
+    </Tab>
+  );
+};
 
-    </div>
- )   
-}
-
-export default Gallery
+export default GalleryTab;
