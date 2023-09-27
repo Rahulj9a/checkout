c5c36e84068bfeee45b7e50ea08a0996389c1556
Rahul Solanki <rahul.solanki.12032001@gmail.com>
2023-09-27 19:50:50 +0530

    feat: sidebar featuring categories

--- app/(routes)/stores/(routes)/[storeId]/(routes)/category/[categoryId]/page.tsx
+++ app/(routes)/stores/(routes)/[storeId]/(routes)/category/[categoryId]/page.tsx
@@ -5,6 +5,8 @@ import CategoryCard from "@/components/ui/category-card"
 import ProductList from "@/components/product-List"
 import getProducts from "@/actions/(products)/getAllProducts"
 import getStoreProducts from "@/actions/(products)/getStoreProducts"
+import SecondaryNav from "@/components/secondary-nav"
+import getStoreCategories from "@/actions/(categories)/getStorecategories"
 
 
 interface CategoryPageProps {
@@ -17,13 +19,19 @@ interface CategoryPageProps {
 const CategoryPage: React.FC<CategoryPageProps> = async ({ params }) => {
     const categoryId = params.categoryId
     const storeId = params.storeId
+    const categories = await getStoreCategories(storeId)
     const category = await getCategory({ storeId, categoryId })
     const products = await getStoreProducts(storeId, {isFeatured:true, categoryId:categoryId})
     return (<div>
         <Navbar routeNav={<RouteNav category={category} />} />
         <div className="pt-16 px-4 md:px-6 lg:px-8">
+
+       
+        <SecondaryNav title="Store's Categories" categories={categories}/>
+        <div  className="pl-48">
             <CategoryCard data={category} board/>
             <ProductList title="Products" items={products}/>
+        </div>
         </div>
     </div>)
 }
